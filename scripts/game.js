2e64a04dfd91c98d0d588a80309157bab5ed98a9
Fergus Stonehouse <fergus.m.stonehouse@gmail.com>
2024-11-09 15:13:31 +0000

    Add tests and the arrays to script

---
+++ scripts/game.js
@@ -0,0 +1,10 @@
+const { tsModuleBlock } = require("@babel/types")
+
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+}
+
+module.exports = { game };
